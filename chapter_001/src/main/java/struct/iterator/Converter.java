4765992c3d3d12085143b1fc6e1f2d257180e2c2
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-04-22 22:07:57 +0300

    5.1.4. Создать convert(Iterator<Iterator>)[#265692]

---
+++ chapter_001/src/main/java/struct/iterator/Converter.java
@@ -0,0 +1,34 @@
+package struct.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<>() {
+            Iterator<Integer> iterator = it.next();
+
+            @Override
+            public boolean hasNext() {
+                if (!iterator.hasNext()) {
+                    if (it.hasNext()) {
+                        iterator = it.next();
+                    }
+                }
+                return iterator.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                return iterator.next();
+            }
+        };
+    }
+
+}
