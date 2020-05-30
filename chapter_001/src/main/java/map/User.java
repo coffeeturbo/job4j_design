b3b07054a2a230ac4e86bd3d0c8e34d7fc3a5f2e
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-05-30 17:27:55 +0300

    2. Не перекрывать equals hashCode [#265707]

--- chapter_001/src/main/java/map/User.java
+++ chapter_001/src/main/java/map/User.java
@@ -3,7 +3,38 @@ package map;
 import java.util.Calendar;
 
 public class User {
-    private String name;
     private int id;
+    private String name;
     private Calendar birthday;
+
+    public User(String name, int id, Calendar birthday) {
+        this.name = name;
+        this.id = id;
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
 }
---
+++ chapter_001/src/main/java/map/UserHandler.java
@@ -0,0 +1,22 @@
+package map;
+
+import java.util.GregorianCalendar;
+
+public class UserHandler {
+
+    public static void main(String[] args) {
+        User first = new User("first", 1, new GregorianCalendar());
+        User second = new User("first", 1, new GregorianCalendar());
+
+//        Map<User, Object> map = new HashMap<>();
+//        map.put(first, new Object());
+//        map.put(second, new Object());
+
+//        System.out.println(first.hashCode());
+//        System.out.println(second.hashCode());
+
+//        for (User user : map.keySet()) {
+//            System.out.println(user.getName());
+//        }
+    }
+}
