5e2717220108c76ffeea6d066514f411257ba49f
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-06-29 12:19:05 +0300

    FileInputStream [#265730] review fix

--- chapter_002/src/main/java/io/EvenNumberFile.java
+++ chapter_002/src/main/java/io/EvenNumberFile.java
@@ -1,1 +1,28 @@
-package io;import java.io.FileInputStream;public class EvenNumberFile {    public static void main(String[] args) {        try (FileInputStream in = new FileInputStream("even.txt")) {            StringBuilder text = new StringBuilder();            int read;            while ((read = in.read()) != -1) {                char readChar = (char) read;                text.append(readChar);            }            // System.lineSeparator() не корректрно срабатыват на маке            String[] words = text.toString().split("\r");            for (String word : words) {                if (Integer.parseInt(word) % 2 == 0) {                    System.out.println(word);                }            }        } catch (Exception e) {            e.printStackTrace();        }    }}
+package io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                char readChar = (char) read;
+                text.append(readChar);
+            }
+
+            // System.lineSeparator() не корректрно срабатыват на маке
+            String[] words = text.toString().split("\r");
+            for (String word : words) {
+
+                if (Integer.parseInt(word) % 2 == 0) {
+                    System.out.println(word);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
