b8ec8ef08df0529129680c7291bac9d106d62d55
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-06-30 18:24:10 +0300

    Читаем файл конфигурации [#265726]

---
+++ chapter_002/src/test/java/io/ConfigTest.java
@@ -0,0 +1,20 @@
+package io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+            config.value("hibernate.connection.username"),
+            is("postgres")
+        );
+    }
+}
