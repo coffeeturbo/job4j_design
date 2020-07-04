63df0e2663828eefd320cb5427d3c9f54084baeb
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-07-04 14:48:37 +0300

    Читаем файл конфигурации [#265726] 2

--- chapter_002/src/main/java/io/Config.java
+++ chapter_002/src/main/java/io/Config.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringJoiner;
 
 public class Config {
     private final String path;
@@ -26,24 +25,13 @@ public class Config {
                 }
             });
         } catch (Exception e) {
-            e.getStackTrace();
+            System.out.println(e.getStackTrace());
         }
 
     }
 
     public String value(String key) {
         return this.values.get(key);
-    }
-
-    @Override
-    public String toString() {
-        StringJoiner out = new StringJoiner(System.lineSeparator());
-        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            read.lines().forEach(out::add);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return out.toString();
     }
 
     public static void main(String[] args) {
