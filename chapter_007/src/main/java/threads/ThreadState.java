c944ff99a9c65fd61765402403ccf063091bfed8
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-12-30 14:03:46 +0300

    Состояние нити. [#438776] review fix

--- chapter_007/src/main/java/threads/ThreadState.java
+++ chapter_007/src/main/java/threads/ThreadState.java
@@ -3,31 +3,25 @@ package threads;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-            () -> {
-                System.out.println(Thread.currentThread().getName());
-            }
+            () -> System.out.println(Thread.currentThread().getName())
         );
         first.start();
 
         Thread second = new Thread(
-            () -> {
-                System.out.println(Thread.currentThread().getName());
-            }
+            () -> System.out.println(Thread.currentThread().getName())
         );
         second.start();
 
         while ((first.getState() != Thread.State.TERMINATED)
-            && (second.getState() != Thread.State.TERMINATED)) {
-            System.out.printf("%s: %s \n", first.getName(), first.getState());
-            System.out.printf("%s: %s \n", second.getName(), second.getState());
+            || (second.getState() != Thread.State.TERMINATED)) {
+            print(first);
+            print(second);
         }
+        print(first);
+        print(second);
+    }
 
-        System.out.printf("%s: %s \n", first.getName(), first.getState());
-        System.out.printf("%s: %s \n", second.getName(), second.getState());
-
-        if ((first.getState() == Thread.State.TERMINATED)
-            && (second.getState() == Thread.State.TERMINATED)) {
-            System.out.println("Executing of Threads has done");
-        }
+    private static void print(Thread thread) {
+        System.out.printf("%s: %s \n", thread.getName(), thread.getState());
     }
 }
