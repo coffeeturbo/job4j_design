1c27c4e7c2df7d1c857b3c70b7aa9ea5ea07113c
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2021-01-03 02:08:40 +0300

    Прерывание блокированной нити. [#438778] review fix

--- chapter_007/src/main/java/threads/coccurent/Wget.java
+++ chapter_007/src/main/java/threads/coccurent/Wget.java
@@ -1,20 +1,51 @@
 package threads.coccurent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            try {
-                var i = 0;
-                while (i <= 100) {
-                    Thread.sleep(1000);
-                    System.out.printf("\r Loading: %s%%", i);
-                    i++;
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
-        thread.start();
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+
+                if (bytesRead > speed) {
+                    System.out.println(bytesRead);
+                    int pauseTime = ((bytesRead - speed) / speed) * 1000;
+                    System.out.println(pauseTime);
+                    Thread.sleep(pauseTime);
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+//        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        int speed = Integer.parseInt(args[1]);
+//        int speed = 512;
+        Thread wget = new Thread(new threads.Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
