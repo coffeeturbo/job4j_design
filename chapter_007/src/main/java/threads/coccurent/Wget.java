5c6feb6028444519616ed89c62b56a939aabb73b
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2021-01-04 17:17:48 +0300

    Скачивание файла с ограничением. [#438775] review fix

--- chapter_007/src/main/java/threads/coccurent/Wget.java
+++ chapter_007/src/main/java/threads/coccurent/Wget.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 
 public class Wget implements Runnable {
     private final String url;
@@ -16,7 +17,6 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
             byte[] dataBuffer = new byte[1024];
@@ -39,13 +39,27 @@ public class Wget implements Runnable {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-//        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        int speed = Integer.parseInt(args[1]);
-//        int speed = 512;
-        Thread wget = new Thread(new threads.Wget(url, speed));
+    public static void main(String[] args) throws IllegalArgumentException, InterruptedException {
+        validateArgs(args);
+        Optional<String> url = Optional.of(args[0]);
+        Optional<Integer> speed = Optional.of(Integer.parseInt(args[1]));
+        Thread wget = new Thread(new Wget(url.get(), speed.get()));
         wget.start();
         wget.join();
+
+    }
+
+    private static void validateArgs(String[] args) throws IllegalArgumentException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Expected two params passed");
+        }
+
+        if (args[0] == null || args[0].isEmpty()) {
+            throw new IllegalArgumentException("param 0 - url is not passed");
+        }
+
+        if (args[1] == null || args[1].isEmpty()) {
+            throw new IllegalArgumentException("param 1 - speed is not passed");
+        }
     }
 }
