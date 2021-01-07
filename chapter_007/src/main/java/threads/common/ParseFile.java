4185ea5968b86fa1da2d306b1f028ea5d1cf75cb
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2021-01-07 16:55:59 +0300

    Visibility. Общий ресурс вне критической секции [#438790] review fix

--- chapter_007/src/main/java/threads/common/ParseFile.java
+++ chapter_007/src/main/java/threads/common/ParseFile.java
@@ -14,36 +14,49 @@ public class ParseFile {
     }
 
     public synchronized String getContent() throws IOException {
-        try (InputStream i = new FileInputStream(file)) {
-            StringBuilder output = new StringBuilder();
-            int data;
-            byte[] dataBuffer = new byte[1024];
-            while ((data = i.read(dataBuffer, 0, 1024)) != -1) {
-                output.append((char) data);
-            }
-            return output.toString();
-        }
+        return getContent(file, new SimpleAppendStrategy());
     }
 
     public synchronized String getContentWithoutUnicode() throws IOException {
-        try (InputStream i = new FileInputStream(file)) {
-            StringBuilder output = new StringBuilder();
-            byte[] dataBuffer = new byte[1024];
-            int data;
-            while ((data = i.read(dataBuffer, 0, 1024)) > 0) {
-                if (data < 0x80) {
-                    output.append((char) data);
-                }
-            }
-            i.close();
-            return output.toString();
-        }
+        return getContent(file, new AppendWithoutUnicode());
     }
 
     public synchronized void saveContent(String content) throws IOException {
         try (OutputStream o = new FileOutputStream(file)) {
             for (int i = 0; i < content.length(); i += 1) {
                 o.write(content.charAt(i));
+            }
+        }
+    }
+
+    private String getContent(File file, AppendStrategy strategy) throws IOException {
+        try (InputStream i = new FileInputStream(file)) {
+            StringBuilder output = new StringBuilder();
+            int data;
+            byte[] dataBuffer = new byte[1024];
+            while ((data = i.read(dataBuffer, 0, 1024)) != -1) {
+                strategy.execute(output, data);
+            }
+            return output.toString();
+        }
+    }
+
+    private abstract class AppendStrategy {
+        abstract void execute(StringBuilder output, int data);
+    }
+
+    private class SimpleAppendStrategy extends AppendStrategy {
+        @Override
+        public void execute(StringBuilder output, int data) {
+            output.append((char) data);
+        }
+    }
+
+    private class AppendWithoutUnicode extends AppendStrategy {
+        @Override
+        void execute(StringBuilder output, int data) {
+            if (data < 0x80) {
+                output.append((char) data);
             }
         }
     }
